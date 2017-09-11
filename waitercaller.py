7f50dd21d2f079d96385ad4423a7a1a10e1647c4
fgrullon <franfelix@gmail.com>
2017-09-11 09:22:08 -0400

    debug

--- waitercaller.py
+++ waitercaller.py
@@ -42,7 +42,7 @@ def login():
 		if stored_user and PH.validate_password(form.loginpassword.data,
 		stored_user['salt'], stored_user['hashed']):
 		    user = User(form.loginemail.data)
-            login_user(user, remember=True)
+		    login_user(user, remember=True)
 		    return redirect(url_for('account'))
 		form.loginemail.errors.append("Email or password invalid")
 	return render_template("home.html", loginform=form,
