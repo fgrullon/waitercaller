c3d0d5d858f42a94cabb8f3ba9d7e177b80993ef
fgrullon <franfelix@gmail.com>
2017-09-11 10:26:34 -0400

    debug

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import PasswordField
 from wtforms import SubmitField
 from wtforms.fields.html5 import EmailField
@@ -6,7 +6,7 @@ from wtforms import validators
 from wtforms import TextField
 
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
 	email = EmailField('email',
 	validators = [validators.DataRequired(), validators.Email()])
 	password = PasswordField('password',
@@ -17,14 +17,14 @@ class RegistrationForm(Form):
 	validators.EqualTo('password', message="Passwords must match")])
 	submit = SubmitField('submit', [validators.DataRequired()])
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
 	loginemail = EmailField('email',
 	validators = [validators.DataRequired(), validators.Email()])
 	loginpassword = PasswordField('password',
 	validators = [validators.DataRequired(message="Password is required")])
 	submit = SubmitField('submit', [validators.DataRequired()])
 
-class CreateTableForm(Form):
+class CreateTableForm(FlaskForm):
 	tablenumber = TextField('tablenumber',
 	validators = [validators.DataRequired()])
 	submit = SubmitField('createtablesubmit',
